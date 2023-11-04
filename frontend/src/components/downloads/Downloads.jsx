d483cd30e42221c945cff54096cea22eab80af3a
Umayangi Ekanayake <umayangi1999@gmail.com>
2023-11-04 22:47:36 +0530

    Update Downloads.jsx

--- frontend/src/components/downloads/Downloads.jsx
+++ frontend/src/components/downloads/Downloads.jsx
@@ -11,10 +11,8 @@ const Downloads = () => {
             <div className="card">
               <h3>Project Proposal</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
-                fugit distinctio eum ipsam repellendus sit numquam laborum ipsum
-                fugiat consequatur optio, pariatur tempora veniam quas inventore
-                quos earum quam temporibus.
+                The project proposal is included here. By clicking the buttons
+                below, you can successfully download the documents
               </p>
 
               <div className="button-container">
@@ -33,10 +31,8 @@ const Downloads = () => {
             <div className="card">
               <h3>Status document 1</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
-                fugit distinctio eum ipsam repellendus sit numquam laborum ipsum
-                fugiat consequatur optio, pariatur tempora veniam quas inventore
-                quos earum quam temporibus.
+                The Status document 1 is included here. By clicking the button
+                below, you can successfully download the document
               </p>
               <button className="btn">
                 <a href="https://drive.google.com/drive/folders/1nJ9z-lXzAtclqXFVcmKdocZp0BD5C6H0?usp=sharing">
@@ -47,10 +43,8 @@ const Downloads = () => {
             <div className="card">
               <h3>Progress Presentation 1</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
-                fugit distinctio eum ipsam repellendus sit numquam laborum ipsum
-                fugiat consequatur optio, pariatur tempora veniam quas inventore
-                quos earum quam temporibus.
+                The Progress Presentation 1 is included here. By clicking the
+                button below, you can successfully download the document
               </p>
               <button className="btn">
                 <a href="https://drive.google.com/drive/folders/1T0BuC511PctFDbztFKawTaiKIHe7eH2f?usp=sharing">
@@ -61,10 +55,8 @@ const Downloads = () => {
             <div className="card">
               <h3>Research Paper</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
-                fugit distinctio eum ipsam repellendus sit numquam laborum ipsum
-                fugiat consequatur optio, pariatur tempora veniam quas inventore
-                quos earum quam temporibus.
+                The Research Paper is included here. By clicking the buttons
+                below, you can successfully download the documents
               </p>
               <button className="btn">
                 <a href="https://drive.google.com/drive/folders/1GfEPxq5P_OOqo8FyHA2ciZJljMooKz5A?usp=sharing">
@@ -80,10 +72,8 @@ const Downloads = () => {
             <div className="card">
               <h3>Progress Presentation 2</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
-                fugit distinctio eum ipsam repellendus sit numquam laborum ipsum
-                fugiat consequatur optio, pariatur tempora veniam quas inventore
-                quos earum quam temporibus.
+                The Progress Presentation 2 is included here. By clicking the
+                button below, you can successfully download the document
               </p>
               <button className="btn">
                 <a href="https://drive.google.com/drive/folders/1jZj5Bc-Z9zTsOhzIdXu_Yq74QKJCyKSf?usp=sharing">
@@ -94,10 +84,8 @@ const Downloads = () => {
             <div className="card">
               <h3>Poster</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
-                fugit distinctio eum ipsam repellendus sit numquam laborum ipsum
-                fugiat consequatur optio, pariatur tempora veniam quas inventore
-                quos earum quam temporibus.
+                The Poster is included here. By clicking the button below, you
+                can successfully download the poster
               </p>
               <button className="btn">
                 <a href="https://drive.google.com/drive/folders/1mKNgCaonmgHVHfCh6p95lOgog41Tnj6H?usp=sharing">
@@ -108,10 +96,8 @@ const Downloads = () => {
             <div className="card">
               <h3>Final Presentation</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
-                fugit distinctio eum ipsam repellendus sit numquam laborum ipsum
-                fugiat consequatur optio, pariatur tempora veniam quas inventore
-                quos earum quam temporibus.
+                The Final Presentation is included here. By clicking the button
+                below, you can successfully download the document
               </p>
               <button className="btn">
                 <a href="https://drive.google.com/drive/folders/1yVl14kONnU2CFRZk6YExZWUoOk25gR3P?usp=sharing">
@@ -122,10 +108,8 @@ const Downloads = () => {
             <div className="card">
               <h3>Final Report</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
-                fugit distinctio eum ipsam repellendus sit numquam laborum ipsum
-                fugiat consequatur optio, pariatur tempora veniam quas inventore
-                quos earum quam temporibus.
+                The Final Report is included here. By clicking the buttons
+                below, you can successfully download the documents
               </p>
               <button className="btn">
                 <a href="https://drive.google.com/drive/folders/1k4-vEOGoZnIkvvyHPp_3z8TBtLNrswJj?usp=sharing">
@@ -141,10 +125,8 @@ const Downloads = () => {
             <div className="card">
               <h3>Status document 2</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
-                fugit distinctio eum ipsam repellendus sit numquam laborum ipsum
-                fugiat consequatur optio, pariatur tempora veniam quas inventore
-                quos earum quam temporibus.
+                The Status document 2 is included here. By clicking the button
+                below, you can successfully download the document
               </p>
               <button className="btn">
                 <a href="https://drive.google.com/drive/folders/1iHtWda3mtqWs-OjO7s-e8KyHBZ1riBI-?usp=sharing">
